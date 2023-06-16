6904f73ec78a0c9425c2b35079cd3deb7c12258a
OlyaDa <dashevska.olga@gmail.com>
2023-06-17 02:22:37 +0300

    add block for speedometer

--- js/main.js
+++ js/main.js
@@ -7,30 +7,3 @@ selects.forEach(el => {
         searchEnabled: false,
     });
 });
-
-//go to top button
-const scrollBtn = document.querySelector('.go-to-top');
-const coords = document.documentElement.clientHeight;
-window.onscroll = () => {
-    if (window.scrollY > coords) {
-        scrollBtn.classList.add('go-to-top--show');
-    } else {
-        scrollBtn.classList.remove('go-to-top--show');
-    }
-}
-scrollBtn.onclick = () => {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-    });
-}
-
-
-//Applications
-window.onload = function () {
-    document.body.classList.add('loaded_hiding');
-    window.setTimeout(function () {
-        document.body.classList.add('loaded');
-        document.body.classList.remove('loaded_hiding');
-    }, 500);
-}
